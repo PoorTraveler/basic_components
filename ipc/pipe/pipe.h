6762e2c40831fdc5af7832a20e4998183aeb48cc
Xiaopeng Tian <tianxiaopeng@nullmax.ai>
2020-12-14 17:35:21 +0800

    add pipe

---
+++ ipc/pipe/pipe.h
@@ -0,0 +1,29 @@
+#ifndef IPC_PIPE_HEADER_FILE
+#define IPC_PIPE_HEADER_FILE
+
+#include <unistd.h>
+#include <assert.h>
+
+namespace ipc{
+namespace unnamedpipe{
+
+class Pipe  {
+ public:
+  Pipe();
+  ~Pipe();
+  bool Open();
+  ssize_t Write(void* msg, size_t count);
+  ssize_t Read(void* buf, size_t count);
+  void CloseWriteEnd();
+  void CloseReadEnd();
+  void Close();
+  
+ private:
+  // fd_array_[0]:  read
+  // fd_array_[1]:  write
+  int fd_array_[2] = {-1, -1};
+};
+
+} //namespace unnamedpipe
+} //namespace ipc
+#endif
